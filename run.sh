c19768cc692add1bbcdeba93b6da0779f7d4bbe3
Kim, Joo-Ho <teamsprint@gmail.com>
2020-01-16 23:24:13 +0900

    fit with other scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+IMAGE_NAME=teamsprint/mysql:5.7
 CONTAINER_NAME=mysql
 
-sudo docker run -d -it --name $CONTAINER_NAME -p 18088:8088 -p 18042:8042 -p13306:3306 --privileged teamsprint/mysql:5.7 /usr/sbin/init
+sudo docker run -d -it --name $CONTAINER_NAME -p 18088:8088 -p 18042:8042 -p13306:3306 --privileged $IMAGE_NAME /usr/sbin/init
 sudo docker exec -it $CONTAINER_NAME /bin/bash
 
